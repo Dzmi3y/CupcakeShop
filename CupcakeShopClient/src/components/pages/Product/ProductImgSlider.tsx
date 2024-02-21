0773bb3662643a4a518336dee71d2aa58c75350a
Dzmitry <dzmi3y@gmail.com>
2024-02-21 12:13:59 +0300

    add primary markup to ProductPage

---
+++ CupcakeShopClient/src/components/pages/Product/ProductImgSlider.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ProductImgSlider = () => {
+  return (
+    <div>ProductImgSlider</div>
+  )
+}
--- CupcakeShopClient/src/components/pages/Product/ProductPage.tsx
+++ CupcakeShopClient/src/components/pages/Product/ProductPage.tsx
@@ -2,6 +2,67 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../../hooks";
 import { getAdditionalParams, getDetailProductInfo, getRecommendedProducts } from "../../../store/reducers/detailProductReducer";
 import { useEffect } from "react";
+import styled from "styled-components";
+import { BreadCrumbs, BreadCrumbsItem } from "../../common/BreadCrumbs";
+import { ProductImgSlider } from "./ProductImgSlider";
+import { Dropdown } from "../../common/Dropdown";
+import { ProductDetailDescription } from "./ProductDetailDescription";
+
+const Container = styled.main`
+  margin: 0 2%;
+`;
+
+const HeadContainer = styled.div`
+
+`;
+
+const ImgSliderContainer = styled.div`
+
+`;
+
+const MainContentContainer = styled.div`
+
+`;
+
+const BreadCrumbsContainer = styled.div`
+  margin: 2rem 0 0 2%;
+  @media (min-width: 958px) {
+        margin: 2rem 0 0 8%;
+  }
+`;
+
+const Title = styled.div`
+  font-size: var(--text-size-extra-large);
+  font-family: var(--font-family-light);
+`;
+
+const Price = styled.div`
+  font-size: var(--text-size-large);
+  font-family: var(--font-family-light);
+`;
+
+const OrderButton = styled.button`
+  background-color: var(--color-dark);
+  color: var(--color-light);
+  font-size: var(--text-size-large);
+  font-family: var(--font-family-light);
+  width: 287px;
+  height: 68px;
+  border-radius: 10px;
+`;
+
+const ShortDescriptionList = styled.ul`
+  &::marker{
+        color: var(--color-dark);
+        font-size: 2rem;
+    }
+`;
+
+const ShotrDescriptionItem = styled.li`
+  text-transform: lowercase;
+  font-size: var(--text-size-medium);
+  font-family: var(--font-family-light);
+`;
 
 
 export const ProductPage = () => {
@@ -11,16 +72,65 @@ export const ProductPage = () => {
   const detailProductStore = useAppSelector(state => state.detailProductStore);
   const dispatch = useAppDispatch();
 
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(getDetailProductInfo(Number(id)));
     dispatch(getAdditionalParams(Number(id)));
-    dispatch(getRecommendedProducts({id:Number(id),count:4}));
-  },[dispatch]);
+    dispatch(getRecommendedProducts({ id: Number(id), count: 4 }));
+  }, [dispatch]);
 
+  let breadCrumbsList: BreadCrumbsItem[] = [];
+  if (detailProductStore.productInfo) {
+    breadCrumbsList = [
+      { title: "Home", link: "/" },
+      { title: "Catalog", link: "/catalog" },
+      { title: detailProductStore.productInfo.name, link: null }
+    ];
+  }
+
+  const OrderButtonClickHandler = () => {
+    /*To Do*/
+    console.log("order button clicked")
+  }
 
 
 
   return (
-    <div>ProductPage</div>
+
+    <Container>
+      <HeadContainer>
+        <ImgSliderContainer>
+
+          <ProductImgSlider/>
+
+        </ImgSliderContainer>
+        <MainContentContainer>
+
+            <Title></Title>
+
+            <ShortDescriptionList>
+              <ShotrDescriptionItem> </ShotrDescriptionItem>
+            </ShortDescriptionList>
+
+
+                <Dropdown/>
+
+                <Dropdown/>
+
+                <Dropdown/>
+
+
+                <Price/>
+
+                <OrderButton>To order</OrderButton>
+
+        </MainContentContainer>
+      </HeadContainer>
+
+      <ProductDetailDescription/>
+
+      <BreadCrumbsContainer>
+        <BreadCrumbs breadCrumbsList={breadCrumbsList} />
+      </BreadCrumbsContainer>
+    </Container>
   )
 }
